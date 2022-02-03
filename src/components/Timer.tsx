0a7ff451a9af8c02259ae162e403e6c07e55278c
idubrovskaya <irina.dubrovskaya95@gmail.com>
2022-02-03 23:51:56 +0300

    added homework (timer)

---
+++ src/components/Timer.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import styles from './Timer.module.css';
+
+export const Timer = () => {
+  const [counter, setCounter] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  const toggle = () => {
+    setIsActive(!isActive);
+  };
+
+  const onClickReset = () => {
+    setCounter(0);
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    let interval: any = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setCounter(counter + 1);
+      }, 1000);
+    } else if (!isActive && counter !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, counter]);
+
+  return (
+    <>
+      <p className={styles.p}>{counter}</p>
+      <button className={styles.button} onClick={toggle}>
+        Start
+      </button>
+      <button onClick={toggle}>Stop</button>
+      <button onClick={onClickReset}>Reset</button>
+    </>
+  );
+};
